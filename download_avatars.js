d0004d285ae2df46f6d2531eedd4de3a7e8f934e
Arthur Di Lascio <arthur.dilascio@gmail.com>
2017-12-04 20:50:52 -0800

    done!

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,4 @@
+//Declaring var's
 var request = require('request');
 var token = require('./secrets');
 var http = require('http');
@@ -6,20 +7,26 @@ var owner = process.argv[2];
 var name = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+//Download avatars by URL (URL and filePath to save it).
 function downloadImageByURL(url, filePath) {
+//sending request
   return request.get(url)
          .on('error', function (err) {
            if(err) {
+            //throw error if has any.
              throw err;
            }
          })
          .on('response', function (response){
            console.log('Response status code: ', response.statusCode);
+          //verifying status code 200 OK
            console.log('Downloading avatar!');
          })
+        //copying and saving avatar
         .pipe(fs.createWriteStream(filePath));
 }
-
+//identifying all repositor contributors.
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -29,17 +36,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
     },
     json: true
   };
-    
+    //downloading each avatar
   request(options, function(err, res, body) {
     body.forEach((item) => {
       downloadImageByURL(item.avatar_url, `./avatars/${item.login}.jpg`);
     });
   });
 }
+//executing function
 getRepoContributors(owner, name, function(err, result) {
   console.log("Errors:", err);
   console.log(JSON.stringify(result, null, 2));
 });
-
-
-//downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
