c105cedbde5a31b78bc0f32ace780546108919f5
Arthur Di Lascio <arthur.dilascio@gmail.com>
2017-12-04 19:48:07 -0800

    step 6 done!

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,27 @@
 var request = require('request');
+var token = require('./secrets');
+var http = require('http');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': "token " + token.GITHUB_TOKEN
+    },
+    json: true
+  };
+    
+  request(options, function(err, res, body) {
+    body.forEach((item) => {
+      console.log(item.avatar_url);
+      cb(err, body);
+    });
+  });
+}
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log(JSON.stringify(result, null, 2));
+});
